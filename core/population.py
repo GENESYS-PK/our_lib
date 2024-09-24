7c4ad60d2243d6f8f09f0c2e6d31ad0c15e9b674
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 20:36:15 +0200

    feat: implement Population class

--- core/population.py
+++ core/population.py
@@ -1,2 +1,27 @@
+from dataclasses import dataclass, field
+from typing import List
+import singledispatch
+
+@dataclass
 class Population:
-    pass
+    population: List[Individual] = field(default_factory=list)
+
+    @property
+    def population_size(self) -> int:
+        return len(self.population)
+
+    @singledispatch
+    def add_to_population(individual: Individual):
+        self.population.append(individual)
+
+    @singledispatch
+    def add_to_population(individual_list: Iterable[Individual]):
+        self.population.extend(individual_list)
+
+    @singledispatch
+    def add_to_population(individual_list: Population):
+        self.population.extend(individual_list.population)
+
+    def trim_children(how_many_keep: int):
+        self.population = self.population[:how_many_keep]
+        
