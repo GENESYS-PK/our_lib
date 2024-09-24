1fb53c97a42b4a5a1ab2998a971fe36ac142a3d4
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:47:16 +0200

    feat: implement mutate method

--- core/mutation.py
+++ core/mutation.py
@@ -1,3 +1,4 @@
+import numpy as np
 from abc import ABC, abstractmethod
 
 class Mutation(ABC):
@@ -6,6 +7,14 @@ class Mutation(ABC):
         self.allowedRepresentation = Representation()
 
     def mutate(self, population_parent: Population) -> Population:
+        offspring = Population()
+
+        for individual in population_parent.population:
+            if np.random.rand() < self.probability:
+                offspring.add_to_population(self._mutate(individual, population_parent))
+
+        offspring.trim_children(population_parent.population_size)
+        return offspring
         
     @abstractmethod
     def _mutate(self, individual: Individual, population: Population) -> Population:
