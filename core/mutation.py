18b47f4fbcfc0efb5352cc271c6f675046f6b1c1
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:44:55 +0200

    feat: add abstraction to Mutation class

--- core/mutation.py
+++ core/mutation.py
@@ -1,11 +1,13 @@
-class Mutation:
+from abc import ABC, abstractmethod
+
+class Mutation(ABC):
     def __init__(self, probability: float = 0):
         self.probability = probability
         self.allowedRepresentation = Representation()
 
     def mutate(self, population_parent: Population) -> Population:
-        pass
-
+        
+    @abstractmethod
     def _mutate(self, individual: Individual, population: Population) -> Population:
         pass
 
