16ca81d0442ec64fe1ebb9cc245ffff7e5f6d686
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:24:17 +0200

    feat: add constructor for Crossover class

--- core/crossover.py
+++ core/crossover.py
@@ -1,2 +1,8 @@
 class Crossover:
-    pass
+    def __init__(self, how_many_individuals: int, probability: float = 0):
+        self.how_many_individuals = how_many_individuals
+        self.probability = probability
+        self.allowedRepresentation = Representation()
+
+    
+    
