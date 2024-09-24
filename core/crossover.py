f680abd5e13716ec052e077f00c1f64c728adc3a
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:36:38 +0200

    feat: implement cross method

--- core/crossover.py
+++ core/crossover.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Crossover:
     def __init__(self, how_many_individuals: int, probability: float = 0):
         self.how_many_individuals = how_many_individuals
@@ -5,7 +8,14 @@ class Crossover:
         self.allowedRepresentation = Representation()
 
     def cross(self, population_parent: Population) -> Population:
-        pass
+        offspring = Population()
+
+        while offspring.population_size < population_parent.population_size:
+            if np.random.rand() < self.probability:
+                offspring.add_to_population(self._cross(population_parent))
+
+        offspring.trim_children(self.how_many_individuals)
+        return offspring
 
     def _cross(self, population_parent: Population) -> Population:
         pass
