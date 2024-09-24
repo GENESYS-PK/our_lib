ce5d91cb5b1d29fd7ad5d0d47431d1398564fbeb
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:41:08 +0200

    feat: add mutate and _mutate methods

--- core/mutation.py
+++ core/mutation.py
@@ -3,4 +3,10 @@ class Mutation:
         self.probability = probability
         self.allowedRepresentation = Representation()
 
+    def mutate(self, population_parent: Population) - > Population:
+        pass
+
+    def _mutate(self, individual: Individual, population: Population) - > Population:
+        pass
+
     
