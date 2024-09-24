0823e579307e2858713ba2545a85a808c622bf3c
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:54:58 +0200

    style: add black formatting

--- core/mutation.py
+++ core/mutation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from abc import ABC, abstractmethod
 
+
 class Mutation(ABC):
     def __init__(self, probability: float = 0):
         self.probability = probability
@@ -13,11 +14,9 @@ class Mutation(ABC):
             if np.random.rand() < self.probability:
                 offspring.add_to_population(self._mutate(individual, population_parent))
 
-        offspring.trim_children(population_parent.population_size)
+        # offspring.trim_children(population_parent.population_size)
         return offspring
-        
+
     @abstractmethod
     def _mutate(self, individual: Individual, population: Population) -> Population:
         pass
-
-    
