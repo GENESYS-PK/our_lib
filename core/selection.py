0b6d703959b86738f3e332c456eb7a09656f35bd
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:04:56 +0200

    feat: add constructor for Selection class

--- core/selection.py
+++ core/selection.py
@@ -1,2 +1,5 @@
 class Selection:
-    pass
+    def __init__(self, target_population: int, maximize: bool):
+        self.target_population = target_population
+        self.maximize = maximize
+        self.allowedRepresentation = Representation()
