33795a17336812ba482d4442fb676163660aa68a
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-24 19:14:19 +0200

    feat: Selection class complete

--- core/selection.py
+++ core/selection.py
@@ -1,11 +1,17 @@
-class Selection:
+from abc import ABC, abstractmethod
+
+class Selection(ABC):
     def __init__(self, target_population: int, maximize: bool):
         self.target_population = target_population
         self.maximize = maximize
         self.allowedRepresentation = Representation()
 
     def select(self, population: Population) -> Population:
-        pass
-    
+        selected_population = self._select(population)
+
+        population.trim_children(self.target_population)
+        return selected_population
+
+    @abstractmethod
     def _select(self, population: Population) -> Population:
         pass
