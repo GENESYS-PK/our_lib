4f31b77247e0d3fc06b3fc90339746e88cf0bcfb
Marcin Fortuna <marcin.fortuna@onet.eu>
2024-09-29 10:35:08 +0200

    docs: add docstrings for Selection class

--- core/selection.py
+++ core/selection.py
@@ -1,7 +1,16 @@
 from abc import ABC, abstractmethod
 from population import Population
 
+
 class Selection(ABC):
+    """
+    An abstract base class that serves as a blueprint for specific selection methods.
+
+    :param target_population: The target population size.
+    :param maximize: A boolean flag that indicates whether the selection method should maximize or minimize the fitness function.
+    :param allowed_representation: A list of allowed representations for the selection operation.
+    """
+
     allowed_representation = []
 
     def __init__(self, target_population: int, maximize: bool):
@@ -9,6 +18,12 @@ class Selection(ABC):
         self.maximize = maximize
 
     def select(self, population: Population) -> Population:
+        """
+        Perform the selection operation on the population.
+
+        :param population: The population to perform the selection operation on.
+        :returns: The selected population.
+        """
         selected_population = self._select(population)
 
         population.trim_children(self.target_population)
@@ -16,4 +31,10 @@ class Selection(ABC):
 
     @abstractmethod
     def _select(self, population: Population) -> Population:
+        """
+        Abstract method that defines the logic for performing selection on a population.
+
+        :param population: The population to perform the selection operation on.
+        :returns: The selected population.
+        """
         pass
