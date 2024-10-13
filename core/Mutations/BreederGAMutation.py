4c491ba0657a8b0615367a72fec04b2a5130257a
Arkadiusz Knapik <arkadiusz.knapik.200@gmail.com>
2024-10-13 22:55:00 +0200

    fix: corrections after comments

--- core/Mutations/BreederGAMutation.py
+++ core/Mutations/BreederGAMutation.py
@@ -1,40 +1,42 @@
 import numpy as np
 from core import Mutation, Individual, Population
+from core.Representation import Representation
+
 
 class BreederGAMutation(Mutation):
-    def __init__(self, probability: float, k: float, ranges: list):
-        """
-        Constructor for the BreederGAMutation class.
+    """
+    Implements Breeder GA mutation where gene values are adjusted based on mutation ranges.
 
-        Parameters:
-            probability (float): Probability of mutation (between 0 and 1).
-            k (float): Mutation precision parameter.
-            ranges (list): List of mutation ranges for each gene.
-        """
+    Algorytmy genetyczne : kompendium. T. 2, Operator mutacji dla problemów numerycznych. / T. D. Gwiazda, 2021 (strona 206)
+
+    :param probability: Probability of mutating each gene in an individual.
+    :param k: Parameter controlling mutation precision.
+    :param ranges: List of mutation ranges for each gene.
+    """
+
+    allowed_representation = [Representation.REAL]
+
+    def __init__(self, k: float, ranges: list, probability: float = 0):
         super().__init__(probability)
         self.k = k
-        self.ranges = ranges
+        self.ranges = np.array(ranges)
 
     def _mutate(self, individual: Individual, population: Population) -> None:
         """
-        Applies Breeder GA mutation to the individual's chromosome.
+        Apply Breeder GA mutation to an individual's chromosome.
 
-        Parameters:
-            individual (Individual): A single individual to mutate.
-            population (Population): The population containing individuals.
-
-        Returns:
-            None: The population with mutated individuals.
+        :param individual: The individual to mutate.
+        :param population: The population containing the individual.
+        :returns: None
         """
         chromosome = individual.chromosome
 
         for i in range(len(chromosome)):
             if np.random.uniform(0, 1) <= self.probability:
                 alfa = np.random.uniform(0, 1)
+                delta = self.ranges[i] * 2 ** (-self.k * alfa)
 
                 if np.random.uniform(0, 1) <= 0.5:
-                    chromosome[i] -= self.ranges[i] * 2 ** (-self.k * alfa)
+                    chromosome[i] -= delta
                 else:
-                    chromosome[i] += self.ranges[i] * 2 ** (-self.k * alfa)
-
-        individual.chromosome = chromosome
+                    chromosome[i] += delta
