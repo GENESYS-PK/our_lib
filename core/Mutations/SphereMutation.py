832f59740c8e904c97c0cbcbfd1f2ddfa6d12f66
shaayy187 <dawid.puchala2002@gmail.com>
2024-10-22 18:44:24 +0200

    Gaussian Crossover & Sphere Mutation Implementation

--- core/Mutations/SphereMutation.py
+++ core/Mutations/SphereMutation.py
@@ -1,5 +1,4 @@
 import numpy as np
-from abc import ABC, abstractmethod
 from core import Population, Mutation
 from core.Individual import Individual
 import random
@@ -7,16 +6,14 @@ import math
 
 
 class SphereMutation(Mutation):
-    def __init__(self, probability: float, psm: float):
+    def __init__(self, probability: float):
         """
         Constructor for the Sphere Mutation class.
 
         Parameters:
             probability (float): Probability of mutation (between 0 and 1).
-            psm (float): The probability of mutating each individual in the population.
         """
         super().__init__(probability)
-        self.psm = psm  # Probability of sphere mutation for each individual
 
     def _mutate(self, individual: Individual, population: Population) -> None:
         """
@@ -32,12 +29,12 @@ class SphereMutation(Mutation):
         self.sphere_mutation(individual.chromosome)
 
     def sphere_mutation(self, chromosome):
-        if np.random.uniform(0, 1) <= self.psm:
+        # Use the mutation probability from the Mutation superclass
+        if np.random.rand() <= self.probability:
 
             k, q = random.sample(range(len(chromosome)), 2)
             a = np.random.uniform(0, 1)
             B = math.sqrt((chromosome[k] / chromosome[q]) ** 2 * (1 - a ** 2) + 1)
-
 
             chromosome[k] = a * chromosome[k]
             chromosome[q] = B * chromosome[q]
